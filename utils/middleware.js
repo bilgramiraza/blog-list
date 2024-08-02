b3584015bcc72a48575b19a1820a879f6cf44f55
Raza <bilgramiraza@yahoo.com>
2024-08-02 12:19:21 +0530

    Added Error Handling Middleware

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,9 +1,21 @@
 const logger = require('./logger');
 
-const unknownEndpoint = (request, response) => {
+const unknownEndpoint = (_request, response) => {
 	response.status(404).send({ error: 'unknown Endpoint' });
 };
 
+const errorHandler = (error, _request, response, next) => {
+	logger.error(error.message);
+	switch (error.name) {
+		case 'CastError':
+			return response.status(400).send({ error: 'malformatted Id' });
+		case 'ValidationError':
+			return response.status(400).send({ error: error.message.split(':')[2] });
+		default: next(error);
+	}
+};
+
 module.exports = {
-	unknownEndpoint
+	unknownEndpoint,
+	errorHandler,
 };
