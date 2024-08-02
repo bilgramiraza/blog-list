ffbca80370727fb4069f7384c38bbabbd11d264f
Raza <bilgramiraza@yahoo.com>
2024-08-02 11:45:02 +0530

    Grouping Tests under categories

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,90 +1,78 @@
-const { test, after, beforeEach } = require('node:test');
+const { describe, test, after, beforeEach } = require('node:test');
 const assert = require('node:assert');
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
 const Blog = require('../models/blogs');
+const helper = require('./test_helper');
 
 const api = supertest(app);
 
-const initialBlogs = [
-	{
-		title: 'test',
-		author: 'tester',
-		url: 'tester.com',
-		likes: 420,
-	},
-	{
-		title: 'test2',
-		author: 'tester',
-		url: 'tester.com',
-		likes: 69,
-	},
-];
-
 beforeEach(async () => {
 	await Blog.deleteMany({});
-	let blogObject = new Blog(initialBlogs[0]);
+	let blogObject = new Blog(helper.initialBlogs[0]);
 	await blogObject.save();
-	blogObject = new Blog(initialBlogs[1]);
+	blogObject = new Blog(helper.initialBlogs[1]);
 	await blogObject.save();
 });
 
-test('Blogs are returned as JSON', async () => {
-	await api
-		.get('/api/blogs')
-		.expect(200)
-		.expect('Content-Type', /application\/json/);
+describe('API Read Operations', () => {
+	test('Blogs are returned as JSON', async () => {
+		await api
+			.get('/api/blogs')
+			.expect(200)
+			.expect('Content-Type', /application\/json/);
+	});
+
+	test('There are Two Blogs', async () => {
+		const response = await api.get('/api/blogs');
+
+		assert.strictEqual(response.body.length, helper.initialBlogs.length);
+	});
+
+	test('The First Blog is Titled \'test\'', async () => {
+		const response = await api.get('/api/blogs');
+
+		const titles = response.body.map(e => e.title);
+		assert(titles.includes('test'));
+	});
 });
 
-test('There are Two Blogs', async () => {
-	const response = await api.get('/api/blogs');
+describe('API Write Operations', () => {
+	test('A Valid Blog is Added', async () => {
+		const newBlog = {
+			title: 'newTest',
+			author: 'newTester',
+			url: 'newTester.com',
+			likes: 69,
+		};
 
-	assert.strictEqual(response.body.length, initialBlogs.length);
-});
+		await api
+			.post('/api/blogs/')
+			.send(newBlog)
+			.expect(201)
+			.expect('Content-Type', /application\/json/);
 
-test('The First Blog is Titled \'test\'', async () => {
-	const response = await api.get('/api/blogs');
+		const blogs = await helper.blogsInDB();
+		assert.strictEqual(blogs.length, helper.initialBlogs.length + 1);
 
-	const titles = response.body.map(e => e.title);
-	assert(titles.includes('test'));
-});
+		const titles = blogs.map(b => b.title);
+		assert(titles.includes('newTest'));
+	});
 
-test('A Valid Blog is Added', async () => {
-	const newBlog = {
-		title: 'newTest',
-		author: 'newTester',
-		url: 'newTester.com',
-		likes: 69,
-	};
+	test('Invalid Blog Won\'t be added', async () => {
+		const newBlog = {
+			likes: 69,
+		};
 
-	await api
-		.post('/api/blogs/')
-		.send(newBlog)
-		.expect(201)
-		.expect('Content-Type', /application\/json/);
+		await api
+			.post('/api/blogs/')
+			.send(newBlog)
+			.expect(400);
 
-	const response = await api.get('/api/blogs');
-
-	const titles = response.body.map(e => e.title);
-
-	assert.strictEqual(response.body.length, initialBlogs.length + 1);
-	assert(titles.includes('newTest'));
-});
-
-test('Invalid Blog Won\'t be added', async () => {
-	const newBlog = {
-		likes: 69,
-	};
-
-	await api
-		.post('/api/blogs/')
-		.send(newBlog)
-		.expect(400);
-
-	const response = await api.get('/api/blogs');
-
-	assert.strictEqual(response.body.length, initialBlogs.length);
+		const blogs = await helper.blogsInDB();
+		assert.strictEqual(blogs.length, helper.initialBlogs.length);
+	});
 });
 
 after(async () => {
